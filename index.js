e153c7cf6eb7ad036dba72fb12c755958440ed4b
iremcos <i.cos@sintlucasedu.nl>
2023-03-22 10:49:31 +0100

    6

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 document.getElementById("Main").innerText = "Point And Click Adventure";
+
+const offcetCharacter = 16;
+
+const mainCharachter = document .getElementById("mainCharachter")
+const GameWindow = document .getElementById("GameWindow")
+
+GameWindow.onClick = function (e){
+    var rect = GameWindow. getBoundingClientRect{};
+    var x = e.Clientx - rest.left;
+    var y = e.Clienty - rest.top
+    mainCharachter.style.left = x + "px"';
+    mainCharachter.style.top = y + "px";
+
+
+}
